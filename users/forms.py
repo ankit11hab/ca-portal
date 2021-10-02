2223cd4c7911a3333852be5ff141b89dae3f56df
danshCode <soumadipdas18@yahoo.com>
2021-10-02 17:13:32 +0530

    new form added

--- users/forms.py
+++ users/forms.py
@@ -1,30 +1,37 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from .models import UserGroup,NewUser
+from .models import UserGroup, NewUser
 from django.contrib.auth import get_user_model
 from phonenumber_field.formfields import PhoneNumberField
 User = get_user_model()
 
 
 class SingleUserRegisterForm(UserCreationForm):
-    email = forms.EmailField(label="Email")
     firstname = forms.CharField(label="Full Name")
     referred_by = forms.CharField(label="Enter referral id")
-    phone = PhoneNumberField(widget=forms.TextInput(), label="Phone number (e.g. +12125552368)", required=False)
-
+    phone = PhoneNumberField(widget=forms.TextInput(
+    ), label="Phone number (e.g. +12125552368)", required=False)
+    widgets = {
+        'email': forms.EmailInput(attrs={'class': 'input_field', 'placeholder': 'Email'}),
+    }
     class Meta:
         model = User
-        fields = ['firstname', 'email', 'phone', 'graduation_year', 'college_state', 'college_city', 'college_name', 'position_of_responsibility', 'interested_modules','referred_by']
+        fields = ['firstname', 'email', 'phone', 'graduation_year', 'college_state', 'college_city',
+                  'college_name', 'position_of_responsibility', 'interested_modules', 'referred_by']
+
+    
 
 
 class GroupUserRegisterFormForSingle(forms.ModelForm):
     email = forms.EmailField(label="Email")
     firstname = forms.CharField(label="Full Name")
-    phone = PhoneNumberField(widget=forms.TextInput(), label="Phone number (e.g. +12125552368)", required=False)
+    phone = PhoneNumberField(widget=forms.TextInput(
+    ), label="Phone number (e.g. +12125552368)", required=False)
 
     class Meta:
         model = User
-        fields = ['firstname', 'email', 'phone', 'graduation_year', 'position_of_responsibility', 'interested_modules']
+        fields = ['firstname', 'email', 'phone', 'graduation_year',
+                  'position_of_responsibility', 'interested_modules']
 
 
 # incomplete
@@ -37,8 +44,10 @@ class GroupUserRegisterForm(forms.ModelForm):
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField(label="Email")
     firstname = forms.CharField(label="Full Name")
-    phone = PhoneNumberField(widget=forms.TextInput(), label="Phone number (e.g. +12125552368)", required=False)
+    phone = PhoneNumberField(widget=forms.TextInput(
+    ), label="Phone number (e.g. +12125552368)", required=False)
 
     class Meta:
         model = User
-        fields = ['firstname', 'email', 'phone', 'graduation_year', 'college_state', 'college_city', 'college_name',  'position_of_responsibility', 'interested_modules']
+        fields = ['firstname', 'email', 'phone', 'graduation_year', 'college_state',
+                  'college_city', 'college_name',  'position_of_responsibility', 'interested_modules']
