bc5edd7f103ba5205a99727300f92a836d0c4b4f
Ankit Guha <75986219+ankit11hab@users.noreply.github.com>
2021-10-05 17:19:08 +0530

    Points added

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import ShareablePost
+from subIdea.models import Tasks
 
 def dashboard(request):
     if request.user.is_authenticated:
         post_list = ShareablePost.objects.all().order_by('-created_on')
+        tasks_done = Tasks.objects.filter(user=request.user.id)
         context = {
             'post_list': post_list,
+            'tasks_done':tasks_done,
         }
         return render(request, 'dashboard/dashboard_page.html',context)
     else:
