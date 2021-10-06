3f2465b36fe8725aa5615664ddf210e3da46820f
danshCode <soumadipdas18@yahoo.com>
2021-10-06 16:59:08 +0530

    messages customized

--- ca_portal/settings.py
+++ ca_portal/settings.py
@@ -142,12 +142,12 @@ USE_L10N = True
 USE_TZ = True
 
 MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-secondary',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger',
- }
+        messages.DEBUG: 'secondary',
+        messages.INFO: 'info',
+        messages.SUCCESS: 'success',
+        messages.WARNING: 'warning',
+        messages.ERROR: 'error',
+}
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
