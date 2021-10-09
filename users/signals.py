dbdd9166cf67daf428ea040527bf116bf5716b52
danshCode <soumadipdas18@yahoo.com>
2021-10-09 20:16:42 +0530

    major fixes

--- users/signals.py
+++ users/signals.py
@@ -5,42 +5,44 @@ from dashboard.models import Notifications
 
 @receiver(pre_save, sender=NewUser)
 def user_pre_save(sender, instance, **kwargs):
-    if instance.id is None:
+    if instance is None:
         pass
-
     else:
         current = instance
-        previous = NewUser.objects.get(id=instance.id)
+        try:
+            previous = NewUser.objects.get(id=instance.id)
         # Points change
-        if previous.points != current.points:
-            notif = Notifications()
-            notif.message = "Your points increased from " + str(previous.points)+" to "+str(current.points)
-            notif.user=instance
-            notif.type="Success"
-            notif.save()
+            if previous.points != current.points:
+                notif = Notifications()
+                notif.message = "Your points increased from " + str(previous.points)+" to "+str(current.points)
+                notif.user=instance
+                notif.type="Success"
+                notif.save()
 
-            if current.points>100:
+                if current.points>100:
                 # Reached silver
-                notif = Notifications()
-                notif.message = "Congratualations, you reached Silver level"
-                notif.user = instance
-                notif.type = "Success"
-                notif.save()
+                    notif = Notifications()
+                    notif.message = "Congratualations, you reached Silver level"
+                    notif.user = instance
+                    notif.type = "Success"
+                    notif.save()
 
-            if current.points>1000:
+                if current.points>1000:
                 #Reached Gold
-                notif = Notifications()
-                notif.message = "Congratualations, you reached Gold level"
-                notif.user = instance
-                notif.type = "Success"
-                notif.save()
+                    notif = Notifications()
+                    notif.message = "Congratualations, you reached Gold level"
+                    notif.user = instance
+                    notif.type = "Success"
+                    notif.save()
         # Referral
-        if previous.referrals != current.referrals:
-            notif = Notifications()
-            notif.message = "Congratualations, someone used your AlcherID to register. Your points are updated"
-            notif.user = instance
-            notif.type = "Success"
-            notif.save()
-            
+                if previous.referrals != current.referrals:
+                    notif = Notifications()
+                    notif.message = "Congratualations, someone used your AlcherID to register. Your points are updated"
+                    notif.user = instance
+                    notif.type = "Success"
+                    notif.save()
+        except:
+            pass
+
 
         
