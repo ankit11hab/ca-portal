0e31e7e1aeba6c01e141cc45dfe0fbb2537a12ef
Digvijay <digvijaysihag123@gmail.com>
2022-08-08 23:55:32 +0530

    group leaderboard and time wise points upon liking the post

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,21 +1,25 @@
 from logging import currentframe
+from time import timezone
 from django.http.response import HttpResponse
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Promotions, ShareablePost
 from users.models import UserGroup, NewUser
 from submissions.models import Media
-from datetime import datetime
 from django.db.models import Exists,OuterRef
+import datetime
+from datetime import datetime
 from submissions.models import Idea
 from .models import Notifications
+from django.utils import timezone
 from django.db.models import Q
 from django.contrib import messages
 from django.core.paginator import Paginator
+import time
+  
 
-
-""" start_time = _datetime.datetime.now().date()
-user_name = 'a64guha'
+# start_time = datetime.now().date()
+"""user_name = 'a64guha'
 password = 'Ankit@123#'
 api = Client(user_name, password) """
 
@@ -68,7 +72,6 @@ def dashboard(request):
         return render(request, 'dashboard/dashboard_page.html',context)
     else:
         return render(request, 'dashboard/landing_page.html')
-
 
 @login_required
 def contactus(request):
@@ -125,14 +128,20 @@ def leaderboard(request):
             isread = False
             break
     users = NewUser.objects.all().order_by('-points')
-    paginator = Paginator(users,5)
-    page_number = request.GET.get('page')
-    page_obj= paginator.get_page(page_number)
+    groupUsers=UserGroup.objects.all().order_by('-leader')
+    paginator1 = Paginator(users,5)
+    paginator2 = Paginator(groupUsers,5)
+    page_number1 = request.GET.get('page')
+    page_number2 = request.GET.get('page')
+    page_obj1= paginator1.get_page(page_number1)
+    page_obj2= paginator2.get_page(page_number2)
     context = {
         'heading': "Leaderboard",
         'users': users,
+        'grpusers': groupUsers,
         'notification_list': notification_list, 'isread': isread,
-        'index': page_obj
+        'index': page_obj1,
+        'index1':page_obj2,
     }
     return render(request, 'dashboard/complete_leaderboard.html', context)
 
@@ -140,47 +149,79 @@ def leaderboard(request):
 def verify_like(request):
 
     
-    """ print(start_time)
+    # print(start_time)
     post = ShareablePost.objects.get(id=str(list(request.GET.keys())[0]))
-    check = 1
-    if post.likedusers != '':
-        arr = post.likedusers.split()
-        for item in arr:
-            if item==request.user.instahandle:
-                check=0
-                break
-    if check == 0:
-        messages.error(request,"You have already liked this post!")
+    curr_time = time.localtime() 
+    curr_time1 = time.strftime("%H:%M:%S") 
+    created_on=post.created_on
+    created_on1=created_on.strftime("%H:%M:%S")
+    last_date=post.last_date.strftime("%H:%M:%S")
+    timeformat = "%H:%M:%S"
+    delta = (datetime.strptime(curr_time1, timeformat) - datetime.strptime(created_on1, timeformat))
+    diff=delta.seconds
+    # check = 1
+    # if post.likedusers != '':
+    #     arr = post.likedusers.split()
+    #     for item in arr:
+    #         if item==request.user.instahandle:
+    #             check=0
+    #             break
+    # if check == 0:
+    #     messages.error(request,"You have already liked this post!")
+    # else:
+    #     #api = auth.login_instagram('fun_tas_tic_12','Qwerty@123')
+        
+    # curr_time=datetime.now().date()
+    #     delta = curr_time-start_time
+    #     if delta.days >50:
+    #         user_name = 'a64guha'
+    #         password = 'Ankit@123#'
+    #         api2 = Client(user_name, password)
+    #         results = api2.media_likers_chrono(post.media_id)
+    #     else:
+    #         results = api.media_likers_chrono(post.media_id)
+    #     items = results['users']
+        # flag=0
+        
+    #     for item in items: 
+    #         print(item['username'])
+    #         if item['username'] == request.user.instahandle:
+    #             flag=1
+    #             break
+    if flag==1:
+            # if curr_time1<last_date:
+                if(diff<3600):
+                    request.user.points+=25
+                    request.user.tasks+=1
+                    request.user.save()
+                    messages.success(request,"Thank you for liking this post! You have gained 25 points")
+                    post.likedusers+=request.user.instahandle+' '
+                    post.save()
+                elif(diff<7200):
+                    request.user.points+=15
+                    request.user.tasks+=1
+                    request.user.save()
+                    messages.success(request,"Thank you for liking this post! You have gained 15 points")
+                    post.likedusers+=request.user.instahandle+' '
+                    post.save()
+                elif(diff<9800):
+                    request.user.points+=10
+                    request.user.tasks+=1
+                    request.user.save()
+                    messages.success(request,"Thank you for liking this post! You have gained 10 points")
+                    post.likedusers+=request.user.instahandle+' '
+                    post.save()
+                else:
+                    request.user.points+=5
+                    request.user.tasks+=1
+                    request.user.save()
+                    messages.success(request,"Thank you for liking this post! You have gained 5 points")
+                    post.likedusers+=request.user.instahandle+' '
+                    post.save()
+
+
     else:
-        #api = auth.login_instagram('fun_tas_tic_12','Qwerty@123')
-        
-        curr_time=_datetime.datetime.now().date()
-        delta = curr_time-start_time
-        if delta.days >50:
-            user_name = 'a64guha'
-            password = 'Ankit@123#'
-            api2 = Client(user_name, password)
-            results = api2.media_likers_chrono(post.media_id)
-        else:
-            results = api.media_likers_chrono(post.media_id)
-        items = results['users']
-        flag=0
-        
-        for item in items: 
-            print(item['username'])
-            if item['username'] == request.user.instahandle:
-                flag=1
-                break
-        
-        if flag==1:
-            request.user.points+=25
-            request.user.tasks+=1
-            request.user.save()
-            messages.success(request,"Thank you for liking this post! You have gained 25 points")
-            post.likedusers+=request.user.instahandle+' '
-            post.save()
-        else:
-            messages.warning(request,"Looks like you have not liked this post yet!") """
+        messages.warning(request,f"Looks like you have not liked this post yet!")
     return redirect('dashboard_page') 
 
 @login_required
@@ -193,3 +234,4 @@ def notif_unread(request):
                 notif.isread=True
                 notif.save()
         return HttpResponse("OK")
+
