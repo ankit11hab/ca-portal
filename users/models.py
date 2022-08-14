00682039300380d9751a3695773c84640c9f9d33
Siddhant <103687316+Sid2728@users.noreply.github.com>
2022-08-14 17:28:47 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -65,7 +65,7 @@ class NewUser(AbstractBaseUser, PermissionsMixin):
     interested_modules = models.CharField(
         max_length=200, unique=False, blank=True)
     instahandle = models.CharField(
-        max_length=200, blank=True, unique=True, )
+        max_length=200, blank=True, null=True, unique=True, )
     # Referral fields
     referred_by = models.CharField(
         max_length=9, blank=True)
@@ -146,8 +146,11 @@ class UserGroup(models.Model):
     college_state = models.CharField(max_length=200, unique=False)
     college_city = models.CharField(max_length=200, unique=False)
     college_name = models.CharField(max_length=200, unique=False)
+
+    @property
     def getPoints(self):
         return self.leader.points + self.executive.points
+       
 
     referred_by = models.CharField(
         max_length=9, blank=True)
