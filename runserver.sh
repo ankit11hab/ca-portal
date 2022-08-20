74911d660cc2be4c5c4250fc8a578a227a6c6c60
imhrishib <hey.there.hrishikesh@gmail.com>
2022-08-21 04:33:51 +0530

    requirements

--- runserver.sh
+++ runserver.sh
@@ -3,7 +3,5 @@ pip install git+https://git@github.com/ping/instagram_private_api.git@1.6.0
 python manage.py collectstatic --no-input --clear
 #python manage.py createsuperuser --noinput --username alchercaadmin --email alcher@gmail.com
 python manage.py migrate --run-syncdb
-python manage.py makemigrations
-python manage.py migrate
 # python manage.py createsuperuser --noinput --firstname admin --email admin@admin.com
 gunicorn ca_portal.wsgi:application --bind 0.0.0.0:80 --log-level=debug --timeout 180  --workers 4
