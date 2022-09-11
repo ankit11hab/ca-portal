7e6a79179506954b6f0d915f2be4d58c3ef43aab
Digvijay <digvijaysihag123@gmail.com>
2022-09-11 20:37:36 +0530

    insta like

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from instagram_private_api import Client, ClientCompatPatch
+from instagram_private_api import Client, ClientCompatPatch,ClientLoginError,errors
+import random 
 
-user_name = 'fake27_28'
+# user_name = 'fake27_28'
 password = 'sid1234'
 
 short_code = 'ChOgwVQLY4m'
@@ -9,8 +10,17 @@ media_id = 0
 for letter in short_code:
     media_id = (media_id*64) + alphabet.index(letter)
 
-api = Client(user_name, password)
+insta_id_list1 = ["fake27_28","digvijay.s5548","fake275_28"]
+for id in insta_id_list1:
+    api = Client(id,password)
+    if not(api):
+        insta_id_list1.remove(id)
+        api=Client(id, password)
+    else:
+        print("working")
+        break
+
 results = api.media_likers_chrono(media_id)
 # print(results['users'])
-for user in results['users']:
-    print(user['username'])
+# for user in results['users']:
+#     print(user['username'])
---
+++ test2.py
@@ -0,0 +1,36 @@
+import random 
+# user_name='';
+# password='';
+insta_id_list = [
+    {
+        "user_name":"digvijay",
+        # "password" : "1234",
+    },
+    {
+        "user_name":"digvijay1",
+        # "password":"12344",
+    },
+    {
+        "user_name":"digvijay154",
+        # "password":"12344",
+    },
+    {
+        "user_name":"digvijay18787",
+        # "password":"12344",
+    },
+    {
+        "user_name":"digvijay17878",
+        # "password":"12344",
+    },
+    {
+        "user_name":"digvijay198989",
+        # "password":"12344",
+    },
+    {
+        "user_name":"digvijay190",
+        # "password":"12344",
+    },
+]
+
+randomID = random.choice(insta_id_list)
+print(randomID)
