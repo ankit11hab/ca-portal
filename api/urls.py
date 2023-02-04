ffaaacc6bdc0c7607813832ac0921f903a633db6
ankit11hab <ankit11hab@gmail.com>
2023-02-04 10:29:25 +0530

    api

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('referal_id/', views.points_referal_id,name='points-referal'), # api takes alcherid and points in data
+    path('get_decrypted_data/', views.get_decrypted_data,name='get_decrypted_data'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from users.models import NewUser
+import html.parser
 
 @api_view(['PUT'])
 def points_referal_id(request):
@@ -11,6 +12,29 @@ def points_referal_id(request):
         user.save()
         # print('success')
         return Response('success')
+    else:
+        # print('error')
+        return Response('error')
+
+
+def decrypt_data(encrypted_data, key):
+    """Decrypt the data using XOR encryption and a given key"""
+    data = bytearray(len(encrypted_data))
+    key_len = len(key)
+    for i in range(len(encrypted_data)):
+        data[i] = encrypted_data[i] ^ key[i % key_len]
+    return bytes(data)
+
+
+@api_view(['GET'])
+def get_decrypted_data(request):
+    if(request.data.get('encrypted_data')):
+        encrypted_data = request.data.get('encrypted_data')
+        encrypted_data = str.encode(html.unescape(encrypted_data))
+        key = b'mysecretkey'
+        decrypted_data = decrypt_data(encrypted_data, key).decode()
+        # print('success')
+        return Response(decrypted_data)
     else:
         # print('error')
         return Response('error')
